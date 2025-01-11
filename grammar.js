90a446f92ffd833ff6b205ce7964802adf28cfbc
MgShepherd <michaelshep1504@gmail.com>
2025-01-11 13:42:02 +0000

    Added grammer support for assignment, if and whie statements

--- grammar.js
+++ grammar.js
@@ -14,21 +14,51 @@ module.exports = grammar({
     source_file: ($) => $.block,
     block: ($) => seq("{", repeat(seq($._statement, ";")), "}"),
 
-    _statement: ($) => choice($.assignment_statement, $.print_statement),
-    assignment_statement: ($) =>
+    _statement: ($) =>
+      choice(
+        $.declaration_statement,
+        $.assignment_statement,
+        $.print_statement,
+        $.while_statement,
+        $.if_statement,
+      ),
+    declaration_statement: ($) =>
       seq($.datatype, $.identifier, $.assignment_operator, $._expression),
+    assignment_statement: ($) =>
+      seq($.identifier, $.assignment_operator, $._expression),
     print_statement: ($) => seq("print", $.string_value),
+    while_statement: ($) => seq("while", $.comparison_expression, $.block),
+    if_statement: ($) =>
+      seq(
+        "if",
+        $.comparison_expression,
+        $.block,
+        repeat($.elif_statement),
+        optional($.else_statement),
+      ),
+    elif_statement: ($) => seq("elif", $.comparison_expression, $.block),
+    else_statement: ($) => seq("else", $.block),
 
-    _expression: ($) => choice($.value_expression, $.arithmetic_expression),
-    value_expression: ($) => $.numeric_value,
+    _expression: ($) =>
+      choice(
+        $.value_expression,
+        $.arithmetic_expression,
+        $.comparison_expression,
+      ),
+    value_expression: ($) => choice($._value, $.identifier),
     arithmetic_expression: ($) =>
       prec.left(1, seq($._expression, $.arithmetic_operator, $._expression)),
+    comparison_expression: ($) =>
+      prec.left(1, seq($._expression, $.comparison_operator, $._expression)),
+
+    _value: ($) => choice($.numeric_value, $.string_value),
+    numeric_value: ($) => /[0-9]+/,
+    string_value: ($) => /"[a-zA-Z0-9 ]+"/,
 
     datatype: ($) => "i16",
     identifier: ($) => /[a-z]+/,
-    numeric_value: ($) => /[0-9]+/,
-    string_value: ($) => /"[a-zA-Z0-9 ]+"/,
     assignment_operator: ($) => choice("=", "+=", "-=", "*=", "/="),
     arithmetic_operator: ($) => choice("+", "-", "/", "*"),
+    comparison_operator: ($) => choice("<", ">", "<=", ">=", "=="),
   },
 });
